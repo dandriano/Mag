d552f103acc8c8830a891152105369ac0712b245
dandriano <dandrianov@outlook.com>
2024-09-29 19:18:43 +0300

    Add basic bot logic
    There's need to refactor it to buisnes logic

--- Infrastructure/BotInfrastructure.cs
+++ Infrastructure/BotInfrastructure.cs
@@ -5,6 +5,8 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Mag.Infrasctructure
 {
@@ -27,17 +29,39 @@ namespace Mag.Infrasctructure
         {
             _logger.LogInformation("Starting Telegram Bot...");
 
-            var offset = 0; // Initial offset for updates
+            var offset = 0;
             using var timer = new PeriodicTimer(TimeSpan.FromSeconds(1));
             do
             {
-                _logger.LogInformation("Gather Updates...");
                 var updates = await _botClient.GetUpdatesAsync(offset, cancellationToken: cancellationToken);
 
                 foreach (var update in updates)
                 {
                     _logger.LogInformation($"Serving update:\t{update.Type}");
-                    offset = update.Id + 1; // Update offset to avoid processing the same update again
+                    offset = update.Id + 1;
+                    if (update.Type != UpdateType.Message || update.Message == null || string.IsNullOrEmpty(update.Message.Text))
+                        continue;
+
+
+                    var chatId = update.Message.Chat.Id;
+                    switch (update.Message.Text)
+                    {
+                        case "/start":
+                            await _botClient.SendTextMessageAsync(chatId, "Welcome! Choose an option:", cancellationToken: cancellationToken);
+                            break;
+                        case "/menu":
+                            var inlineKeyboard = new InlineKeyboardMarkup(
+                            [
+                                InlineKeyboardButton.WithCallbackData("Option 1", "option1"),
+                                InlineKeyboardButton.WithCallbackData("Option 2", "option2"),
+                            ]);
+
+                            await _botClient.SendTextMessageAsync(chatId, "Choose an option:", replyMarkup: inlineKeyboard, cancellationToken: cancellationToken);
+                            break;
+                        default:
+                            await _botClient.SendTextMessageAsync(chatId, "Unknown command. Try /menu.", cancellationToken: cancellationToken);
+                            break;
+                    }
                 }
             } while (await timer.WaitForNextTickAsync(cancellationToken));
 
